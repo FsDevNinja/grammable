09e804e043e372f9db25b1a1939e4ade4a329b56
Matt Taylor <makproductions06@gmail.com>
2016-10-09 19:32:43 -0400

    gram validaitons

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,26 +1,27 @@
 class GramsController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
     
   end
-
+  
   def new
     @gram = Gram.new
   end
-
+  
   def create
+    @gram = current_user.grams.create(gram_params)
     @gram= Gram.create(gram_params)
     if @gram.valid?
-    redirect_to root_path
-  else
-    render :new, status: :unprocessable_entity
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-  end
-
-end
-
-private
-
- def gram_params
+  
+  private
+  
+  def gram_params
     params.require(:gram).permit(:message)
   end
+end
