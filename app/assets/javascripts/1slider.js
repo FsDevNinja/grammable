7386583da4cca48eab5f3dac14f2cb7b33955373
Matt Taylor <makproductions06@gmail.com>
2016-10-13 18:43:07 -0400

    refactored functions and variables

--- app/assets/javascripts/1slider.js
+++ app/assets/javascripts/1slider.js
@@ -45,17 +45,35 @@
       
       $(parent.sliderGram).height(parent.customHeight);
       
+      //setting rightTrigger
+      var rightTrigger = (parent.singleImageWidth * (numberOfImages - 3)* -1);
+      console.log(rightTrigger);
+      //storing wrapper position value
+      var wrapper_offset = null;
+      
+      //setting leftTrigger
+      var leftTrigger = (parent.singleImageWidth * -2);
+      console.log(leftTrigger);
+      
+      var slideIndex = null;
+      var currentSlide = null;
+      var contentFade = $('.content-fade');
+      
+      
+      
+      
       
       /*****************************************************
       ***************   Show slider of grams ***************
       *****************************************************/
       
       $('.gram').on("click", function() {
-        var slideIndex = $(this).data("index");
+        var slideIndex = parseInt($(this).attr("data-index")) + 1;
+        console.log(slideIndex)
         $('.gram-content').animate({top: '450',opacity : 1}, "fast");
         $('.grid-grams').animate({top: 2000,opacity : 0}, "fast");
         $('.slider-outer').animate({top: 0, opacity : 1},"slow");
-        parent.wrapper.animate({"left" : "-" + (parent.singleImageWidth * 3)  + "px"}, 0);
+        parent.wrapper.animate({"left" : "-" + (parent.singleImageWidth * slideIndex )  + "px"}, 0);
         
       });
       
@@ -66,11 +84,13 @@
       var linkHeight = parent.leftLink.height();
       var leftGramHeight = parent.sliderInner.height();
       
-      parent.leftLink.css({"top" : ((leftGramHeight - linkHeight)/2) + "px"}).on("click", function() {
-        parent.slideRight.call(parent);
+      parent.leftLink.css({"top" : ((leftGramHeight - linkHeight)/2) + "px"});
+      parent.leftLink.on("click", function() {
+        slideRight(parent, leftTrigger, wrapper_offset, numberOfImages, slideIndex, currentSlide, contentFade);
       });
-      parent.rightLink.css({"top" : ((leftGramHeight - linkHeight)/2) + "px"}).on("click", function() {
-        parent.slideLeft(parent);
+      parent.rightLink.css({"top" : ((leftGramHeight - linkHeight)/2) + "px"});
+      parent.rightLink.on("click", function() {
+        slideLeft(parent, rightTrigger, wrapper_offset, numberOfImages, slideIndex, currentSlide, contentFade);
       });
       
       // end of slider Initialization function
@@ -95,41 +115,47 @@
     ***************   Slide Left Function ***************
     *****************************************************/
     
-    gramSlider.prototype.slideLeft(para) = function() {
-      var parent = this;
-      //setting rightTrigger
-      var rightTrigger = (parent.singleImageWidth * (numberOfImages - 4)* -1);
-      //storing wrapper position value
-      var wrapper_offset = (parseInt($(".wrapper").css("left").replace("px","")));
+    function slideLeft(parent, rightTrigger, wrapper_offset, numberOfImages,slideIndex, currentSlide, contentFade){
+      
+      wrapper_offset = (parseInt($(".wrapper").css("left").replace("px","")));
+      console.log(wrapper_offset);
+      console.log(rightTrigger);
       if ( wrapper_offset == rightTrigger  ) {
+        
         parent.wrapper.animate({"left" : "-=" + parent.singleImageWidth + "px"}, parent.waitTime);
-        parent.wrapper.animate({"left" : "-" + (parent.singleImageWidth * 3)  + "px"}, 0);
+        parent.wrapper.animate({"left" : "-" + (parent.singleImageWidth * 2)  + "px"}, 0);
       } else {
         parent.wrapper.animate({"left" : "-=" + parent.singleImageWidth + "px"}, parent.waitTime);
       }
+      
       // fade out gram content
+      contentFade.fadeOut(250);
+      console.log(slideIndex);
+      
       
       // grab new gram content based on index
-      
+      currentSlide = $('.center-wrap').find('[data-index ='slideIndex']');
+      $('gram-description').text = currentSlide.attr('data-message');
       //fade in gram content
+      contentFade.fadeIn(500);
+      slideIndex += 1;
     };
     
     /*****************************************************
     ***************   Slide Right Function ***************
     *****************************************************/
     
-    gramSlider.prototype.slideRight = function() {
-      var parent = this;
-      //setting leftTrigger
-      leftTrigger = (parent.singleImageWidth * -1);
-      //storing wrapper position value
-      var wrapper_offset = (parseInt($(".wrapper").css("left").replace("px","")));
-      // testing if we are at a wrapper end point.
+    function slideRight(parent, leftTrigger, wrapper_offset, numberOfImages, currentSlide) {
+      
+      
+      wrapper_offset = (parseInt($(".wrapper").css("left").replace("px","")));
+      console.log(wrapper_offset)
+      console.log(leftTrigger);
       if ( wrapper_offset == leftTrigger  ) {
         //if true animate to clone
         parent.wrapper.animate({"left" : "+=" + parent.singleImageWidth + "px"}, parent.waitTime);
         //then snap to nonclone version with no transition
-        parent.wrapper.animate({"left" : "-" + (parent.singleImageWidth * (numberOfImages - 6) )  + "px"}, 0);
+        parent.wrapper.animate({"left" : "-" + (parent.singleImageWidth * (numberOfImages - 3) )  + "px"}, 0);
       } else {
         //if false just animate as usual
         parent.wrapper.animate({"left" : "+=" + parent.singleImageWidth + "px"}, parent.waitTime);
